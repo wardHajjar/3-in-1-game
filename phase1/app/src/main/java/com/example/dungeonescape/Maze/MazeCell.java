408572f39558ec0cec76fd00a32c052e33dcc14b
Georgia Bains <georgia.bains@mail.utoronto.ca>
2019-10-27 13:44:34 -0400

    Created file MazeCell.java which represents a square cell in the Maze.

---
+++ phase1/app/src/main/java/com/example/dungeonescape/Maze/MazeCell.java
@@ -0,0 +1,62 @@
+package com.example.dungeonescape.Maze;
+
+import com.example.dungeonescape.GameObject;
+
+/**
+ * Represents a MazeCell GameObject which is a square cell in the Maze.
+ */
+public class MazeCell extends GameObject {
+
+    /** Assigns whether or not this cell as a wall on the specified side. */
+    private boolean topWall = true;
+    private boolean bottomWall = true;
+    private boolean leftWall = true;
+    private boolean rightWall = true;
+
+    /** If this cell has been visited before. */
+    private boolean visited = false;
+
+    public MazeCell(int col, int row, int depth) {
+        super(col, row, depth);
+    }
+
+    public boolean isTopWall() {
+        return topWall;
+    }
+
+    public void setTopWall(boolean topWall) {
+        this.topWall = topWall;
+    }
+
+    public boolean isBottomWall() {
+        return bottomWall;
+    }
+
+    public void setBottomWall(boolean bottomWall) {
+        this.bottomWall = bottomWall;
+    }
+
+    public boolean isLeftWall() {
+        return leftWall;
+    }
+
+    public void setLeftWall(boolean leftWall) {
+        this.leftWall = leftWall;
+    }
+
+    public boolean isRightWall() {
+        return rightWall;
+    }
+
+    public void setRightWall(boolean rightWall) {
+        this.rightWall = rightWall;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+}
