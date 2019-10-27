86c9bcf99216e774373d57b45617e858d4fd0eea
hajjarwa <ward.hajjar@mail.utoronto.ca>
2019-10-27 02:34:40 -0400

    Created the initializer for the Ball Class + Added setter methods for the ball's speed + Changed access modifiers of the BBObject class and its methods to package protected.

--- phase1/app/src/main/java/com/example/dungeonescape/BrickBreaker/BBObject.java
+++ phase1/app/src/main/java/com/example/dungeonescape/BrickBreaker/BBObject.java
@@ -1,47 +1,54 @@
 package com.example.dungeonescape.BrickBreaker;
 
-/*
-Class that creates a brick breaker object.
- */
-public class BBObject {
-    /*
-    the x and y coordinates of the object's location
+/**
+ * Class that creates a brick breaker object.
+*/
+class BBObject {
+    /**
+     *the x and y coordinates of the object's location
      */
     private int x, y;
 
-    /*
-    Method initializes a brick breaker object with a location at x, y.
+    /**
+     * Method initializes a brick breaker object.
+     *
+     * @param x the x coordinate of the new object's location.
+     * @param y the y coordinate of the new object's location.
      */
-    protected BBObject(int x, int y){
+    BBObject(int x, int y){
         this.x = x;
         this.y = y;
     }
 
-    /*
-    Setter method that changes the x coordinate of the object's location.
+    /**
+     * Setter method that changes the x coordinate of the object's location.
+     *
+     * @param x the new x coordinate of the object's location.
      */
-    protected void setX(int x){
+    void setX(int x){
         this.x = x;
     }
 
-    /*
-    Getter method that returns the x coordinate of the object's location.
+    /**
+     * Getter method that returns the x coordinate of the object's location.
      */
-    protected int getX(){
+    int getX(){
         return this.x;
     }
 
-    /*
-    Setter method that changes the y coordinate of the object's location.
+    /**
+     * Setter method that changes the y coordinate of the object's location.
+     *
+     * @param y the new y coordinate of the object's location.
      */
-    protected void setY(int y){
+    void setY(int y){
         this.y = y;
     }
 
-    /*
-    Getter method that returns the y coordinate of the object's location.
+    /**
+     * Getter method that returns the y coordinate of the object's location.
      */
-    protected int getY(){
+    int getY(){
         return this.y;
     }
 }
--- phase1/app/src/main/java/com/example/dungeonescape/BrickBreaker/Ball.java
+++ phase1/app/src/main/java/com/example/dungeonescape/BrickBreaker/Ball.java
@@ -1,4 +1,43 @@
 package com.example.dungeonescape.BrickBreaker;
 
-public class Ball {
+/**
+ * Class that creates the ball used in the brick breaker game.
+ * Ball has the functionality of bouncing from walls and bricks and breaking breaks.
+ */
+public class Ball extends BBObject{
+    /**
+     * The ball's speed in the x and y directions, respectively.
+     */
+    private int x_speed, y_speed;
+    /**
+     * Initializes the ball at a given centre with a specified speed in the x and y directions.
+     *
+     * @param x_loc the x coordinate of the ball's centre.
+     * @param y_loc the y coordinate of the ball's centre.
+     * @param x_speed the speed of the ball in the x direction.
+     * @param y_speed the speed of the ball in the y direction.
+     */
+    public Ball(int x_loc, int y_loc, int x_speed, int y_speed){
+        super(x_loc, y_loc);
+        this.x_speed = x_speed;
+        this.y_speed = y_speed;
+    }
+
+    /**
+     * Setter method that changes the ball's speed in the x direction.
+     *
+     * @param x_speed the new speed in the x direction.
+     */
+    public void setX_speed(int x_speed) {
+        this.x_speed = x_speed;
+    }
+
+    /**
+     * Setter method that changes the ball's speed in the y direction.
+     *
+     * @param y_speed the new speed in the y direction.
+     */
+    public void setY_speed(int y_speed){
+        this.y_speed = y_speed;
+    }
 }
