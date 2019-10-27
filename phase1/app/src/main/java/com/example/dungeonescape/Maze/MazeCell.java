440e52906ec5c99ed4486b69d9d735dd50bf6fb8
Georgia Bains <georgia.bains@mail.utoronto.ca>
2019-10-27 16:18:50 -0400

    MazeManager.java: moved calculateCellSize to this class.

--- phase1/app/src/main/java/com/example/dungeonescape/Maze/MazeCell.java
+++ phase1/app/src/main/java/com/example/dungeonescape/Maze/MazeCell.java
@@ -90,27 +90,4 @@ public class MazeCell extends GameObject {
     public void setVerticalPadding(float verticalPadding) {
         this.verticalPadding = verticalPadding;
     }
-
-    /** Calculates the cellSize based on the screen's dimensions.
-     *
-     * @param screenWidth the width of the phone screen in pixels.
-     * @param screenHeight the height of the phone screen in pixels.
-     * @param numMazeCols the number of columns this Maze has.
-     * @param numMazeRows the number of rows this Maze has.
-     * @return the calculated cellSize.
-     */
-    public float calculateCellSize(int screenWidth, int screenHeight, int numMazeCols,
-                                   int numMazeRows) {
-        float newCellSize;
-        float screenWidthDivHeight = screenWidth / screenHeight;
-        float mazeColsDivRows = numMazeCols / numMazeRows;
-
-        if (screenWidthDivHeight < mazeColsDivRows) {
-          newCellSize = screenWidth / (numMazeCols + 1);
-        } else {
-          newCellSize = screenHeight / (numMazeRows + 1);
-        }
-
-        return newCellSize;
-    }
 }
