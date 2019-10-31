bad336578a7f4a938af79903c69a5090501c7c22
hajjarwa <ward.hajjar@mail.utoronto.ca>
2019-10-30 20:55:25 -0400

    Broke up the BBView class's functionality so that the new class BBGameManager dealt with the objects directly and BBView just dealt with display.

---
+++ phase1/app/src/main/java/com/example/dungeonescape/brickbreaker/BBGameManager.java
@@ -0,0 +1,124 @@
+package com.example.dungeonescape.brickbreaker;
+
+import com.example.dungeonescape.GameManager;
+
+import java.util.ArrayList;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+
+class BBGameManager extends GameManager {
+
+    /**
+     * ball - the ball object that bounces around and hits bricks.
+     * paddle - the paddle that catches the ball.
+     * bricks - list of all the bricks in the game.
+     */
+    private Ball ball;
+    private Paddle paddle;
+    private ArrayList<Brick> bricks;
+    private int screenX;
+    private int screenY;
+
+    BBGameManager(int screenX, int screenY){
+        super();
+
+        // construct the ball
+        this.ball = new Ball(screenX/2 - 75 + (screenX/2 - 75)/2 - 25,
+                screenY - 100 - 26, 25, -26);
+
+        // construct paddle and bricks
+        paddle = new Paddle(screenX/2 - 75, screenY - 100, screenX/3, 40);
+
+        bricks = new ArrayList<>();
+        int brickWidth = screenX / 6;
+        int brickHeight = screenY / 20;
+        for (int x = 0; x < screenX; x += brickWidth + 5) {
+            for (int y = 10; y < 6 * brickHeight; y += brickHeight + 5) {
+                bricks.add(new Brick(x, y, brickWidth, brickHeight));
+            }
+        }
+        this.screenX = screenX;
+        this.screenY = screenY;
+    }
+
+    void moveBall(){
+        ball.move();
+
+        // Wall Collision Detection
+        char wall_collision = ball.madeWallCollision(screenX, screenY);
+        if ( wall_collision == 'x'){
+            ball.setXSpeed(ball.getXSpeed() * -1);
+        }
+        else if(wall_collision == 'y'){
+            ball.setYSpeed(ball.getYSpeed() * -1);
+        }
+
+        // Brick Collision Detection
+        for (Brick brick: bricks){
+            if (!(brick.getHitStatus())) {
+                String brick_collision = ball.madeRectCollision(brick.getRect());
+                if (brick_collision.equals("x")) {
+                    ball.setXSpeed(ball.getXSpeed() * -1);
+                    brick.changeHitStatus();
+                    break;
+                } else if (brick_collision.equals("y")) {
+                    ball.setYSpeed(ball.getYSpeed() * -1);
+                    brick.changeHitStatus();
+                    break;
+                }
+            }
+        }
+
+        // Paddle Collision Detection
+        String paddle_collision = ball.madeRectCollision(paddle.getRect());
+        if (!(paddle_collision.equals(" "))) {
+            ball.setYSpeed(ball.getYSpeed() * -1);
+            ball.setRandomXSpeed();
+        }
+    }
+
+    void movePaddle(){
+        if (paddle.movingLeft) {
+            paddle.move(-20);
+        } else if (paddle.movingRight) {
+            paddle.move(20);
+        }
+
+        if (paddle.getX() <= 0){
+            paddle.setX(0);
+        }else if (paddle.getX() + paddle.getWidth() >= screenX){
+            paddle.setX(screenX - paddle.getWidth());
+        }
+
+    }
+
+    void movePaddle(MotionEvent event, float xPos){
+        if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            if (xPos < paddle.x){
+                paddle.setMovementDir("left", true);
+            } else if (xPos > paddle.x) {
+                paddle.setMovementDir("right", true);
+            }
+        }
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            paddle.setMovementDir("right", false);
+            paddle.setMovementDir("left", false);
+        }
+    }
+
+    void drawGame(Canvas canvas){
+        // Ball
+        ball.draw(canvas);
+
+        // Paddle
+        paddle.draw(canvas);
+
+        // Bricks
+        for (int i = 0; i < bricks.size(); i++) {
+            if (!bricks.get(i).getHitStatus()) {
+                bricks.get(i).draw(canvas);
+            }
+        }
+    }
+
+}
--- phase1/app/src/main/java/com/example/dungeonescape/brickbreaker/BBView.java
+++ phase1/app/src/main/java/com/example/dungeonescape/brickbreaker/BBView.java
@@ -1,15 +1,8 @@
 package com.example.dungeonescape.brickbreaker;
 
 import android.content.Context;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.util.Log;
 import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-
-import java.util.ArrayList;
 
 import android.view.Display;
 import android.graphics.Point;
@@ -37,11 +30,12 @@ public class BBView extends GameView {
      * paddle - the paddle that catches the ball.
      * bricks - list of all the bricks in the game.
      */
-    Ball ball;
-    Paddle paddle;
-    ArrayList<Brick> bricks;
+//    Ball ball;
+//    Paddle paddle;
+//    ArrayList<Brick> bricks;
     boolean startGame = false;
 
+    BBGameManager manager;
     /**
      * Initializes the surface in the context environment.
      * @param context the environment
@@ -55,22 +49,7 @@ public class BBView extends GameView {
         display.getSize(size);
         this.screenX = size.x;
         this.screenY = size.y;
-
-        // construct the ball
-        this.ball = new Ball(screenX/2 - 75 + (screenX/2 - 75)/2 - 25,
-                screenY - 100 - 26, 25, -26);
-
-        // construct paddle and bricks
-        paddle = new Paddle(screenX/2 - 75, screenY - 100, screenX/3, 40);
-
-        bricks = new ArrayList<>();
-        int brickWidth = screenX / 6;
-        int brickHeight = screenY / 20;
-        for (int x = 0; x < screenX; x += brickWidth + 5) {
-            for (int y = 10; y < 6 * brickHeight; y += brickHeight + 5) {
-                bricks.add(new Brick(x, y, brickWidth, brickHeight));
-            }
-        }
+        manager = new BBGameManager(screenX, screenY);
     }
 
     /**
@@ -78,40 +57,8 @@ public class BBView extends GameView {
      */
     public void update() {
         if (startGame){
-            ball.move();
-        }
-
-        // Wall Collision Detection
-        char wall_collision = ball.madeWallCollision(screenX, screenY);
-        if ( wall_collision == 'x'){
-            ball.setXSpeed(ball.getXSpeed() * -1);
-        }
-        else if(wall_collision == 'y'){
-            ball.setYSpeed(ball.getYSpeed() * -1);
-        }
-
-        // Brick Collision Detection
-        for (Brick brick: bricks){
-            if (!(brick.getHitStatus())) {
-                String brick_collision = ball.madeRectCollision(brick.getRect());
-                if (brick_collision.equals("x")) {
-                    ball.setXSpeed(ball.getXSpeed() * -1);
-                    brick.changeHitStatus();
-                    break;
-                } else if (brick_collision.equals("y")) {
-                    ball.setYSpeed(ball.getYSpeed() * -1);
-                    brick.changeHitStatus();
-                    break;
-                }
-            }
-        }
-
-        // Paddle Collision Detection
-        String paddle_collision = ball.madeRectCollision(paddle.getRect());
-        if (!(paddle_collision.equals(" ")) && startGame) {
-            ball.setYSpeed(ball.getYSpeed() * -1);
-            ball.setRandomXSpeed();
-
+            manager.moveBall();
+            manager.movePaddle();
         }
     }
 
@@ -119,6 +66,7 @@ public class BBView extends GameView {
      *
      */
     public void draw() {
+
         // Make sure the drawing surface is valid or program crashes
         if (holder.getSurface().isValid()) {
             // Lock the canvas ready to draw
@@ -131,22 +79,7 @@ public class BBView extends GameView {
 
             paint.setColor(Color.argb(255,  255, 255, 255));
 
-            // Ball
-            ball.draw(canvas);
-
-            // Paddle
-            paddle.draw(canvas);
-            // while playing/not dead
-            if (playing) paddle.checkBounds(screenX);
-            if (playing) paddle.updateLocation();
-
-            // Bricks
-            for (int i = 0; i < bricks.size(); i++) {
-                if (!bricks.get(i).getHitStatus()) {
-                    bricks.get(i).draw(canvas);
-                }
-            }
-
+            manager.drawGame(canvas);
             // Draws everything to the screen
             holder.unlockCanvasAndPost(canvas);
         }
@@ -155,25 +88,20 @@ public class BBView extends GameView {
     // Dictates the movement of the paddle
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
         if(event.getAction() == MotionEvent.ACTION_DOWN) {
             if (!startGame){
-                ball.move();
                 startGame = true;
             }
             return true;
         }
         if (event.getAction() == MotionEvent.ACTION_MOVE) {
-            float x = event.getX();
-            if (x < paddle.x) {
-                paddle.movingLeft = true;
-            } else if (x > paddle.x) {
-                paddle.movingRight = true;
-            }
+
+            manager.movePaddle(event, x);
             return true;
         }
         if (event.getAction() == MotionEvent.ACTION_UP) {
-            paddle.movingLeft = false;
-            paddle.movingRight = false;
+            manager.movePaddle(event, x);
         }
         return super.onTouchEvent(event);
     }
