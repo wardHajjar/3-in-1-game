3dd44fdea2a96804e4628fb77643faed94764388
hajjarwa <ward.hajjar@mail.utoronto.ca>
2019-11-01 02:12:57 -0400

    Added feature that colour choice does not appear until name is entered, but left it commented out for testing.

--- phase1/app/src/main/java/com/example/dungeonescape/NewGameActivity.java
+++ phase1/app/src/main/java/com/example/dungeonescape/NewGameActivity.java
@@ -9,8 +9,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.graphics.Color;
 import com.example.dungeonescape.brickbreaker.BBMainActivity;
+import android.text.TextWatcher;
+import android.text.Editable;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -22,8 +25,8 @@ public class NewGameActivity extends AppCompatActivity {
     GameManager gameManager;
     GameManager data;
     Player player;
-
     EditText name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,45 +38,52 @@ public class NewGameActivity extends AppCompatActivity {
     }
 
     private void buttons() {
-        EditText name = (EditText) findViewById(R.id.txtSub);
-        String nameText = name.getText().toString();
-        System.out.println(nameText);
-        gameManager.addPlayer(nameText);
-        player = gameManager.getPlayer(nameText);
+        final EditText name = findViewById(R.id.txtSub);
+        final Button enter = findViewById(R.id.Enter);
 
-        final EditText name = (EditText) findViewById(R.id.txtSub);
-        Button enter = (Button) findViewById(R.id.Enter);
-        Button colour1 = findViewById(R.id.colour1);
+        final Button colour1 = findViewById(R.id.colour1);
         colour1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Button enter = findViewById(R.id.Enter);
                 enter.setVisibility(View.VISIBLE);
                 player.setColour(Color.argb(255, 173, 0, 0));
             }
         });
 
-        Button colour2 = findViewById(R.id.colour2);
+        final Button colour2 = findViewById(R.id.colour2);
         colour2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Button enter = findViewById(R.id.Enter);
                 enter.setVisibility(View.VISIBLE);
                 player.setColour(Color.argb(255, 76, 175, 80));
             }
         });
 
-        Button colour3 = findViewById(R.id.colour3);
+        final Button colour3 = findViewById(R.id.colour3);
         colour3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Button enter = findViewById(R.id.Enter);
                 enter.setVisibility(View.VISIBLE);
                 player.setColour(Color.argb(255, 76, 175, 80));
             }
         });
 
-        Button enter = findViewById(R.id.Enter);
+//        name.addTextChangedListener(new TextWatcher() {
+//
+//            public void afterTextChanged(Editable s) {
+//                if (!(name.getText().toString().matches(""))){
+//                    TextView colorPrompt =  (TextView) findViewById(R.id.textView3);
+//                    colorPrompt.setText((CharSequence)("Select Character Colour:"));
+//                    colour1.setVisibility(View.VISIBLE);
+//                    colour2.setVisibility(View.VISIBLE);
+//                    colour3.setVisibility(View.VISIBLE);
+//                }
+//            }
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+//
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+//        });
+
         enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
