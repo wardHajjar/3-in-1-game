812b890f603b36ddd661387aac3625f3fc6772c9
Georgia Bains <georgia.bains@mail.utoronto.ca>
2019-11-01 21:13:23 -0400

    MazeActivity.java & Level2MainActivity.java: edited javadocs & android fixes.

--- phase1/app/src/main/java/com/example/dungeonescape/Maze/MazeActivity.java
+++ phase1/app/src/main/java/com/example/dungeonescape/Maze/MazeActivity.java
@@ -20,11 +20,15 @@ import java.util.Locale;
 public class MazeActivity extends GeneralGameActivity {
     private MazeView mazeView;
 
-    // initial time set in milliseconds
+    /** Initial time set in milliseconds. */
     public long counter = 120000; // 2 min
     // public long counter = 6000; // 5s (for testing)
+
+    /** Minutes and seconds values. */
     long minutes;
     long seconds;
+
+    /** Initializes a Player and GameManager. */
     Player player;
     GameManager gameManager;
 
@@ -32,42 +36,47 @@ public class MazeActivity extends GeneralGameActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maze);
+
+        /* Gather saved data. */
         Intent i = getIntent();
         player = (Player) i.getSerializableExtra("Player");
         gameManager = (GameManager) i.getSerializableExtra("Game Manager");
         mazeView = findViewById(R.id.view);
         mazeView.setPlayer(player);
 
-        // sets the Player colour to the input choice from the New Game page
+        /* Sets the Player colour to the input choice from the New Game page. */
         MazeManager mazeManager = mazeView.getMazeManager();
         mazeManager.setPlayerPaint(player.getColour());
 
-        // go to next game
+        /* Go to next game. Testing only. */
         configureNextButton();
 
-        // countdown to losing the game
-        final TextView countTime=findViewById(R.id.countTime);
+        /* Initializes the countdown to losing the game. */
+        final TextView countTime = findViewById(R.id.countTime);
+        countTime.setTextColor(Color.WHITE);
 
-        // countdown code from: https://www.tutorialspoint.com/how-to-make-a-countdown-timer-in-android
-        // partially edited
+        /* Countdown code from: https://www.tutorialspoint.com/how-to-make-a-countdown-timer-in-android
+          Partially edited.
+         */
         new CountDownTimer(counter, 1000) {
 
             @Override
             public void onTick(long millisUntilFinished) {
-                countTime.setTextColor(Color.WHITE);
                 counter = millisUntilFinished;
                 updateCountDown();
-                countTime.setText(String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds));
+                countTime.setText(String.format(Locale.getDefault(),
+                        "%02d:%02d", minutes, seconds));
                 counter--;
             }
 
             @Override
             public void onFinish() {
-                setContentView(R.layout.activity_maze_lose_life);
-
+                /* Player loses a life when the countdown runs out. */
                 player.loseLife();
                 int playerLivesLeft = player.getNumLives();
 
+                /* Change UI to Lose Life screen. */
+                setContentView(R.layout.activity_maze_lose_life);
                 TextView textView = (TextView) findViewById(R.id.playerLives);
                 textView.setText(String.format(Locale.getDefault(),
                         "You have %d lives left.", playerLivesLeft));
@@ -76,22 +85,25 @@ public class MazeActivity extends GeneralGameActivity {
         }.start();
     }
 
+    /**
+     * Converts milliseconds to minutes and seconds.
+     * Calculation from:
+     * https://codinginflow.com/tutorials/android/countdowntimer/part-2-configuration-changes
+     */
     private void updateCountDown() {
-        /* minutes & seconds calculation from here:
-        https://codinginflow.com/tutorials/android/countdowntimer/part-2-configuration-changes
-         */
         minutes = (counter / 1000) / 60;
         seconds = (counter / 1000) % 60;
     }
 
     private void configureNextButton() {
-        Button nextButton = (Button) findViewById(R.id.nextlvl);
+        Button nextButton = findViewById(R.id.nextlvl);
         nextButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 save(gameManager, player);
-                Intent intent = new Intent(MazeActivity.this, PlatformerMainActivity.class);
+                Intent intent = new Intent(MazeActivity.this,
+                        PlatformerMainActivity.class);
                 intent.putExtra("Player", player);
                 intent.putExtra("Game Manager", gameManager);
                 startActivity(intent);
@@ -100,7 +112,7 @@ public class MazeActivity extends GeneralGameActivity {
     }
 
     private void configureStartOverButton() {
-        Button startOver = (Button) findViewById(R.id.startOver);
+        Button startOver = findViewById(R.id.startOver);
         startOver.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -113,33 +125,40 @@ public class MazeActivity extends GeneralGameActivity {
         });
     }
 
-    public void movePlayerUp(View view){
+    /** Moves the Player up by one MazeCell. */
+    public void movePlayerUp(View view) {
         mazeView.movePlayer("UP");
         checkDoneLevel(view);
     }
-    public void movePlayerDown(View view){
+
+    /** Moves the Player down by one MazeCell. */
+    public void movePlayerDown(View view) {
         mazeView.movePlayer("DOWN");
         checkDoneLevel(view);
     }
-    public void movePlayerLeft(View view){
+
+    /** Moves the Player left by one MazeCell. */
+    public void movePlayerLeft(View view) {
         mazeView.movePlayer("LEFT");
         checkDoneLevel(view);
     }
 
-    public void movePlayerRight(View view){
+    /** Moves the Player right by one MazeCell. */
+    public void movePlayerRight(View view) {
         mazeView.movePlayer("RIGHT");
         checkDoneLevel(view);
     }
 
-    public void checkDoneLevel(View view){
+    /** Checks if the Maze has been completed 3 times. Go to next level if true. */
+    public void checkDoneLevel(View view) {
         if (mazeView.doneLevel())
             nextLevel();
     }
+
     /**
      * User has successfully finished Maze and will now move on to Platformer.
      */
-
-    protected void nextLevel(){
+    protected void nextLevel() {
         save(gameManager, player);
         Intent intent = new Intent(MazeActivity.this, PlatformerMainActivity.class);
         intent.putExtra("Player", player);
