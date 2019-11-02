7e862f91dc843c466b0cf34bbb707a05e219ba10
Georgia Bains <georgia.bains@mail.utoronto.ca>
2019-11-01 21:15:09 -0400

    MazeActivityInstructions.java: edited javadocs.

--- phase1/app/src/main/java/com/example/dungeonescape/Maze/MazeActivityInstructions.java
+++ phase1/app/src/main/java/com/example/dungeonescape/Maze/MazeActivityInstructions.java
@@ -1,43 +1,48 @@
 package com.example.dungeonescape.Maze;
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.example.dungeonescape.GameManager;
 import com.example.dungeonescape.Player;
 import com.example.dungeonescape.R;
 
 public class MazeActivityInstructions extends AppCompatActivity {
 
+    /** Initializes a Player and GameManager. */
     Player player;
     GameManager gameManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maze_instructions);
         setTitle("Level2: Maze");
-        // getting player instance from intent
+
+        /* Gather saved data. */
         Intent i = getIntent();
         player = (Player) i.getSerializableExtra("Player");
         gameManager = (GameManager) i.getSerializableExtra("Game Manager");
+
+        /* Button to start the Maze. */
         configureNextButton();
     }
 
+    /** Creates the Start button to play the Maze. */
     private void configureNextButton() {
         Button nextButton = findViewById(R.id.start);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(com.example.dungeonescape.Maze.MazeActivityInstructions.this, MazeActivity.class);
+                Intent intent = new Intent(
+                        com.example.dungeonescape.Maze.MazeActivityInstructions.this,
+                        MazeActivity.class);
                 intent.putExtra("Player", player);
                 intent.putExtra("Game Manager", gameManager);
                 startActivity(intent);
             }
         });
-
-
     }
 }
