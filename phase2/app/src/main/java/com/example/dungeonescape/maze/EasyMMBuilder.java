019d8d9b2df098bd713c55efeaa3891b334539ae
Gerald Wu <gerald.wu@mail.utoronto.ca>
2019-11-25 18:10:38 -0500

    Added comments and improved JAVADOC.

--- phase2/app/src/main/java/com/example/dungeonescape/maze/EasyMMBuilder.java
+++ phase2/app/src/main/java/com/example/dungeonescape/maze/EasyMMBuilder.java
@@ -1,16 +1,19 @@
 package com.example.dungeonescape.maze;
 
-class EasyMazeManagerBuilder implements MMBuilder {
+class EasyMMBuilder implements MMBuilder {
+        //the MazeManager object which we will modify and return.
         private MazeManager manager;
 
-        public EasyMazeManagerBuilder(){
+        public EasyMMBuilder(){
             manager = new MazeManager();
         }
 
+        // the rows and columns are set to 5 corresponding to the chosen difficulty.
         public void setColumns(){manager.setNumMazeCols(5);}
 
         public void setRows(){manager.setNumMazeRows(5);}
 
+        // return the manager when requested by engineer.
         public MazeManager getManager(){return manager;}
 
 }
