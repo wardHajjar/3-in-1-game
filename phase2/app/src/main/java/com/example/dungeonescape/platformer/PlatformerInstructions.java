0b43f346e9bd595bd760a1b796b8dbdeb7a5fe7b
Georgia Bains <georgia.bains@mail.utoronto.ca>
2019-11-24 23:57:46 -0500

    PlatformerInstructions.java: added Menu code.

--- phase2/app/src/main/java/com/example/dungeonescape/platformer/PlatformerInstructions.java
+++ phase2/app/src/main/java/com/example/dungeonescape/platformer/PlatformerInstructions.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.dungeonescape.activities.GeneralGameActivity;
+import com.example.dungeonescape.activities.MainActivity;
+import com.example.dungeonescape.activities.MenuActivity;
+import com.example.dungeonescape.maze.MazeActivity;
 import com.example.dungeonescape.player.PlayerManager;
 import com.example.dungeonescape.player.Player;
 import com.example.dungeonescape.R;
@@ -16,6 +22,9 @@ public class PlatformerInstructions extends GeneralGameActivity {
 
     Player player;
     PlayerManager playerManager;
+
+    private MenuActivity menuActivity = new MenuActivity();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +35,32 @@ public class PlatformerInstructions extends GeneralGameActivity {
         player = (Player) i.getSerializableExtra("Player");
         playerManager = (PlayerManager) i.getSerializableExtra("Game Manager");
         configureNextButton();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.game_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                // TODO: Make a Dialog box with the premise of the game
+                System.out.println("about");
+            case R.id.help:
+                // TODO: Make a Dialog box with
+                System.out.println("help");
+            case R.id.main_menu: // save game and return to main menu
+                save(playerManager, player);
+                Intent intent = menuActivity.createIntent(PlatformerInstructions.this,
+                        MainActivity.class, playerManager, player);
+                startActivity(intent);
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 
     private void configureNextButton() {
