860908f62422e7a710a10dca760c48f65e05af5a
Georgia Bains <georgia.bains@mail.utoronto.ca>
2019-12-01 15:55:00 -0500

    Sprite.java: removed unnecessary comments.

--- phase2/app/src/main/java/com/example/dungeonescape/maze/Sprite.java
+++ phase2/app/src/main/java/com/example/dungeonescape/maze/Sprite.java
@@ -1,33 +1,9 @@
-//package com.example.dungeonescape.maze;
-//
-//import android.graphics.Canvas;
-//
-//import com.example.dungeonescape.game.Drawable;
-//import com.example.dungeonescape.game.GameObject;
-//
-//class Sprite extends GameObject {
-//    Sprite(){}
-//
-//    /** Draws the PlayerSprite square on the screen.
-//     *
-//     * @param canvas the Canvas to draw the PlayerSprite on.
-//     * @param margin the space around the PlayerSprite square. */
-//    void draw(Canvas canvas, float cellSize, float margin) {
-//        canvas.drawRect(
-//                getX() * cellSize + margin,
-//                getY() * cellSize + margin,
-//                (getX() + 1) * cellSize - margin,
-//                (getY() + 1) * cellSize - margin,
-//                getPaint());
-//    }
-//}
-
 package com.example.dungeonescape.maze;
 
-        import android.graphics.Canvas;
+import android.graphics.Canvas;
 
-        import com.example.dungeonescape.game.Drawable;
-        import com.example.dungeonescape.game.GameObject;
+import com.example.dungeonescape.game.Drawable;
+import com.example.dungeonescape.game.GameObject;
 
 class Sprite extends GameObject implements Drawable {
     private MazeData mazeData;
